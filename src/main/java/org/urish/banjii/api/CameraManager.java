3d3d4a2cf55d936df5ee6e3a89f88242bb4da31a
Uri Shaked <uri@salsa4fun.co.il>
2012-04-21 21:37:02 +0300

    Roughly translate camera coordinates to scene coordinates

--- src/main/java/org/urish/banjii/api/CameraManager.java
+++ src/main/java/org/urish/banjii/api/CameraManager.java
@@ -14,7 +14,7 @@ public class CameraManager implements CameraListener {
 	public void onCameraMovement(int cameraId, int markerId, double x, double y, double z, double[] matrix) {
 		logger.info("Camera " + cameraId + " detected marker " + markerId + " at <" + x + ", " + y + ", " + z + ">");
 		if (scene != null) {
-			scene.setPlayerPosition(markerId, x, y, z);
+			scene.setPlayerPosition(markerId, x/100, y/100, z/100);
 		}
 	}
 
