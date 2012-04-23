2e4dbad60673747795973c3322466ab793e0245c
Uri Shaked <uri@salsa4fun.co.il>
2012-04-24 02:27:34 +0300

    getters & setters for camera objects

--- src/main/java/org/urish/banjii/model/Camera.java
+++ src/main/java/org/urish/banjii/model/Camera.java
@@ -2,7 +2,8 @@ package org.urish.banjii.model;
 
 public class Camera {
 	private final int id;
-	private final boolean active;
+	private boolean active;
+	private boolean calibrating;
 	private double x;
 	private double y;
 
@@ -10,6 +11,42 @@ public class Camera {
 		super();
 		this.id = id;
 		this.active = true;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public boolean isCalibrating() {
+		return calibrating;
+	}
+
+	public void setCalibrating(boolean calibrating) {
+		this.calibrating = calibrating;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
 	}
 
 }
